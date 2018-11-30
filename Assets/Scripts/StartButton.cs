c0654ef46b7dc10ab85ca2535bf91683b7b3c673
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-11-30 14:28:17 +0000

    added 2 new screens

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//allows us to usethe scene loading function
+using UnityEngine.SceneManagement;
+public class StartButton : MonoBehaviour {
+    //this will be called by the button componenet when clicked
+   public void StartGame()
+    {
+        //reset the score
+        PlayerPrefs.DeleteKey("score");
+
+        SceneManager.LoadScene("SampleScene");
+
+    }
+	
+}
---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleButton : MonoBehaviour
+{
+    public void GoToTitle()
+    {
+        //return to title scene
+        SceneManager.LoadScene("Title");
+
+    }
+
+}
